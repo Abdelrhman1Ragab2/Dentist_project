14cf0e1dbcf17b5da475b2f0dd233e9f7505dc23
Abdelrhman ragab <abdelrhman1ragab2@gmail.com>
2022-06-10 15:53:22 +0200

    firebase init done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'UI/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  );
   runApp( MaterialApp(
-      home: splashScrean(),
-  debugShowCheckedModeBanner: false,));
+    home: splashScrean(),
+    debugShowCheckedModeBanner: false,));
 }
 
